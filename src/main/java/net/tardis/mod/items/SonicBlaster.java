0a527b39688a771577bad88042f506ca220851f7
Spectre0987 <spencecd2@gmail.com>
2018-05-04 17:10:52 -0700

    Gave up on Sonic Blaster

--- src/main/java/net/tardis/mod/items/SonicBlaster.java
+++ src/main/java/net/tardis/mod/items/SonicBlaster.java
@@ -1,17 +1,15 @@
 package net.tardis.mod.items;
 
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.tardis.mod.Tardis;
-import net.tardis.mod.blocks.TBlocks;
-import net.tardis.mod.tileentity.TileEntitySonicGun;
+import net.tardis.mod.helpers.Helper;
 
 public class SonicBlaster extends Item {
 	
@@ -22,24 +20,7 @@ public class SonicBlaster extends Item {
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-		if(!worldIn.isRemote) {
-			RayTraceResult ray = worldIn.rayTraceBlocks(playerIn.getPositionVector().addVector(0, playerIn.getEyeHeight(), 0), playerIn.getPositionVector().add(playerIn.getLookVec().scale(120D)),true);
-			if(ray!=null&&ray.typeOfHit.equals(RayTraceResult.Type.BLOCK)&&ray.getBlockPos()!=null) {
-				BlockPos pos=ray.getBlockPos().down().west().north();
-				for(int x=0;x<3;x++) {
-					for(int y=0;y<3;y++) {
-						for(int z=0;z<3;z++) {
-							BlockPos cPos=pos.add(x,y,z);
-							IBlockState oldState=worldIn.getBlockState(cPos);
-							if(oldState.getBlockHardness(worldIn, cPos)>-1&&oldState.getBlock()!=TBlocks.sonic_block) {
-								worldIn.setBlockState(cPos, TBlocks.sonic_block.getDefaultState());
-								((TileEntitySonicGun)worldIn.getTileEntity(cPos)).setBlockState(oldState);
-							}
-						}
-					}
-				}
-			}
-		}
+		
 		return super.onItemRightClick(worldIn, playerIn, handIn);
 	}
 }
