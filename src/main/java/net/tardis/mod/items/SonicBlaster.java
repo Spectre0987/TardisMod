17ab7439c6a81b203f9ea1a1f3b593df8210a254
Spectre0987 <spencecd2@gmail.com>
2018-05-04 17:40:14 -0700

    Finally Fixed The Sonic Blaster

--- src/main/java/net/tardis/mod/items/SonicBlaster.java
+++ src/main/java/net/tardis/mod/items/SonicBlaster.java
@@ -6,10 +6,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.tardis.mod.Tardis;
-import net.tardis.mod.helpers.Helper;
+import net.tardis.mod.packets.MessageSB;
 
 public class SonicBlaster extends Item {
 	
@@ -20,7 +19,12 @@ public class SonicBlaster extends Item {
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-		
+		if(worldIn.isRemote) {
+			RayTraceResult ray=playerIn.rayTrace(64D, 0.5F);
+			if(ray!=null && ray.typeOfHit==RayTraceResult.Type.BLOCK) {
+				Tardis.packet_instance.sendToServer(new MessageSB(ray.getBlockPos()));
+			}
+		}
 		return super.onItemRightClick(worldIn, playerIn, handIn);
 	}
 }
