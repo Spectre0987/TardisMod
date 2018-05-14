860e459b57b33b19c50a534678502fd40e083712
Spectre0987 <spencecd2@gmail.com>
2018-05-13 23:19:35 -0700

    Added Comment on Component Class.

--- src/main/java/net/tardis/mod/common/items/ItemComponent.java
+++ src/main/java/net/tardis/mod/common/items/ItemComponent.java
@@ -4,6 +4,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.tardis.mod.info.CrashType;
 
+/**
+ * All TARDIS Components must extend this class
+ * @author Spectre
+ *
+ */
 public abstract class ItemComponent extends Item{
 
 	public ItemComponent() {}
