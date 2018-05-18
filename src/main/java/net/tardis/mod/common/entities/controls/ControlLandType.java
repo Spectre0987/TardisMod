dcd68ffc7e098309cde8bc5ae24d05544d9eb680
Spectre0987 <spencecd2@gmail.com>
2018-05-18 08:12:02 -0700

    Added Landing Type Control

---
+++ src/main/java/net/tardis/mod/common/entities/controls/ControlLandType.java
@@ -0,0 +1,35 @@
+package net.tardis.mod.common.entities.controls;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+import net.tardis.mod.common.tileentity.TileEntityTardis;
+import net.tardis.mod.util.helpers.Helper;
+
+public class ControlLandType extends EntityControl{
+
+	public ControlLandType(TileEntityTardis tardis) {
+		super(tardis);
+	}
+	
+	public ControlLandType(World world) {
+		super(world);
+		this.setSize(0.0625F, 0.0625F);
+	}
+
+	@Override
+	public Vec3d getOffset() {
+		return Helper.convertToPixels(11.5, -2, 4);
+	}
+
+	@Override
+	public void preformAction(EntityPlayer player) {
+		if(!world.isRemote) {
+			TileEntityTardis tardis=(TileEntityTardis)world.getTileEntity(this.getConsolePos());
+			tardis.setShouldLandOnSurface(tardis.landOnSurface?false:true);
+			player.sendMessage(new TextComponentTranslation("tardis.console.land."+tardis.landOnSurface));
+		}
+	}
+
+}
