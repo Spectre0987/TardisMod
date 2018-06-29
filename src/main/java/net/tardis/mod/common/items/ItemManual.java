f7a2c8a058aa3f52b2cadcbcf4a13fbb4b798df4
Snakefangox <snakefangox@gmail.com>
2018-06-30 09:22:35 +1200

    Added translation key to manual.

--- src/main/java/net/tardis/mod/common/items/ItemManual.java
+++ src/main/java/net/tardis/mod/common/items/ItemManual.java
@@ -5,5 +5,11 @@ public class ItemManual extends ItemBase {
 	public ItemManual() {
 		this.setMaxStackSize(1);
 	}
-
+	
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(new TextComponentTranslation("key.manual"));
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
+	
 }
