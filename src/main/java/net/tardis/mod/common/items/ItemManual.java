2a56640e3dcabf39006a2cb325fecb799629a715
Snakefangox <snakefangox@gmail.com>
2018-06-30 09:25:47 +1200

    Conformed to conventions.

--- src/main/java/net/tardis/mod/common/items/ItemManual.java
+++ src/main/java/net/tardis/mod/common/items/ItemManual.java
@@ -8,7 +8,7 @@ public class ItemManual extends ItemBase {
 	
 	@Override
 	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		tooltip.add(new TextComponentTranslation("item.manual"));
+		tooltip.add(new TextComponentTranslation("manual.help"));
 		super.addInformation(stack, worldIn, tooltip, flagIn);
 	}
 	
