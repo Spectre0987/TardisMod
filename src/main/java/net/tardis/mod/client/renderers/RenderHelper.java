82fec1e4f12c09853b44a386146747557237eb15
Spectre0987 <spencecd2@gmail.com>
2018-07-13 07:30:56 -0700

    Added a Config For BOTI

--- src/main/java/net/tardis/mod/client/renderers/RenderHelper.java
+++ src/main/java/net/tardis/mod/client/renderers/RenderHelper.java
@@ -13,6 +13,7 @@ import net.minecraft.util.math.Vec3d;
 import net.tardis.mod.client.renderers.controls.RenderDoor;
 import net.tardis.mod.client.worldshell.IContainsWorldShell;
 import net.tardis.mod.client.worldshell.RenderWorldShell;
+import net.tardis.mod.proxy.ClientProxy;
 
 public class RenderHelper {
 	
@@ -23,44 +24,49 @@ public class RenderHelper {
 	}
 	
 	public static void renderPortal(RenderWorldShell renderShell, IContainsWorldShell te, float partialTicks, float rotation, @Nullable Vec3d offset, @Nullable Vec3d size) {
-		if(offset == null)offset = new Vec3d(-1, 0, -7);
-		GlStateManager.pushMatrix();
-		Minecraft mc = Minecraft.getMinecraft();
-		GL11.glEnable(GL11.GL_STENCIL_TEST);
-		// Always write to stencil buffer
-		GL11.glStencilFunc(GL11.GL_NEVER, 1, 0xFF);
-		GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_KEEP, GL11.GL_KEEP);
-		GL11.glStencilMask(0xFF);
-		GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
-
-		drawOutline(size);
-
-		// Only pass stencil test if equal to 1(So only if rendered before)
-		GL11.glStencilMask(0x00);
-		GL11.glStencilFunc(GL11.GL_EQUAL, 1, 0xFF);
-
-		// Draw scene from portal view
-		
-		try {
-		GlStateManager.pushMatrix();
-		GlStateManager.rotate(180,0,1,0);
-		GlStateManager.rotate(rotation, 0, 1, 0);
-		mc.entityRenderer.disableLightmap();
-		renderShell.doRender(te, offset.x, offset.y, offset.z, 0, partialTicks);
-		mc.entityRenderer.enableLightmap();
-		GlStateManager.popMatrix();
+		if(ClientProxy.getRenderBOTI()) {
+			if(offset == null)offset = new Vec3d(-1, 0, -7);
+			GlStateManager.pushMatrix();
+			Minecraft mc = Minecraft.getMinecraft();
+			GL11.glEnable(GL11.GL_STENCIL_TEST);
+			// Always write to stencil buffer
+			GL11.glStencilFunc(GL11.GL_NEVER, 1, 0xFF);
+			GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_KEEP, GL11.GL_KEEP);
+			GL11.glStencilMask(0xFF);
+			GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
+	
+			drawOutline(size);
+	
+			// Only pass stencil test if equal to 1(So only if rendered before)
+			GL11.glStencilMask(0x00);
+			GL11.glStencilFunc(GL11.GL_EQUAL, 1, 0xFF);
+	
+			// Draw scene from portal view
+			
+			try {
+			GlStateManager.pushMatrix();
+			GlStateManager.rotate(180,0,1,0);
+			GlStateManager.rotate(rotation, 0, 1, 0);
+			mc.entityRenderer.disableLightmap();
+			renderShell.doRender(te, offset.x, offset.y, offset.z, 0, partialTicks);
+			mc.entityRenderer.enableLightmap();
+			GlStateManager.popMatrix();
+			}
+			catch(Exception e) {}
+	
+			GL11.glDisable(GL11.GL_STENCIL_TEST);
+			
+			// Draw portal stencils so portals wont be drawn over
+			GL11.glColorMask(false, false, false, false);
+			drawOutline(size);
+			
+			//Set things back
+			GL11.glColorMask(true, true, true, true);
+		    GlStateManager.popMatrix();
 		}
-		catch(Exception e) {}
-
-		GL11.glDisable(GL11.GL_STENCIL_TEST);
-		
-		// Draw portal stencils so portals wont be drawn over
-		GL11.glColorMask(false, false, false, false);
-		drawOutline(size);
-		
-		//Set things back
-		GL11.glColorMask(true, true, true, true);
-	    GlStateManager.popMatrix();
+		else {
+			RenderHelper.drawOutline(size);
+		}
 	}
 	
 	public static void renderPortal(RenderWorldShell renderShell, IContainsWorldShell te, float partialTicks) {
