706609ad3d98a81e29018acd7d0e0459a3e5955f
Spectre0987 <spencecd2@gmail.com>
2018-07-14 10:39:59 -0700

    Enabled Depth for SOTO

--- src/main/java/net/tardis/mod/client/worldshell/RenderWorldShell.java
+++ src/main/java/net/tardis/mod/client/worldshell/RenderWorldShell.java
@@ -15,7 +15,6 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.tardis.mod.common.entities.controls.ControlDoor;
 import net.tardis.mod.common.tileentity.TileEntityDoor;
 
 public class RenderWorldShell {
@@ -30,7 +29,7 @@ public class RenderWorldShell {
 			IContainsWorldShell container = (IContainsWorldShell) entity;
 			
 			GlStateManager.pushMatrix();
-			if(entity instanceof TileEntityDoor || entity instanceof ControlDoor) GlStateManager.depthFunc(GL11.GL_ALWAYS);
+			if(entity instanceof TileEntityDoor) GlStateManager.depthFunc(GL11.GL_ALWAYS);
 			
 			bb.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
 
