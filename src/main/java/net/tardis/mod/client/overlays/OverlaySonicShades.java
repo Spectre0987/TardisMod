fd1ecc2d543f6ffabfca765ce2a8c7cabaa79a1c
Sub <craigtbalmer@outlook.com>
2018-07-21 16:10:10 +0100

    More work on sonic shades

--- src/main/java/net/tardis/mod/client/overlays/OverlaySonicShades.java
+++ src/main/java/net/tardis/mod/client/overlays/OverlaySonicShades.java
@@ -2,10 +2,15 @@ package net.tardis.mod.client.overlays;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.tardis.mod.Tardis;
 import net.tardis.mod.common.items.TItems;
 
@@ -13,17 +18,71 @@ import java.awt.*;
 
 public class OverlaySonicShades implements IOverlay {
 
-    Minecraft mc = Minecraft.getMinecraft();
+    private Minecraft mc = Minecraft.getMinecraft();
     private ResourceLocation background = new ResourceLocation(Tardis.MODID, "textures/overlay/shades.png");
+    private ModelPlayer biped = new ModelPlayer(1.0F, false);
+
+    public static void drawEntityOnScreen(int posX, int posY, int scale, Entity ent) {
+        GlStateManager.enableColorMaterial();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float) posX, (float) posY, 50.0F);
+        GlStateManager.scale((float) (-scale), (float) scale, (float) scale);
+        GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.rotate(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.rotate(-135.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.translate(0.0F, 0.0F, 0.0F);
+        RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
+        rendermanager.setRenderShadow(false);
+        rendermanager.renderEntity(ent, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, false);
+        GlStateManager.popMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GlStateManager.disableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+    }
 
     @Override
-    public void render(float partialTicks, ScaledResolution resolution) {
+    public void renderUpdate() {
+
+    }
+
+    @Override
+    public void render(RenderGameOverlayEvent e, float partialTicks, ScaledResolution resolution) {
 
         if (mc.player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == TItems.sonicShades) {
 
             Entity mouseOver = Minecraft.getMinecraft().objectMouseOver.entityHit;
             GlStateManager.pushMatrix();
+
+            //Head
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(mc.displayWidth / 2 - 370, mc.displayHeight / 2 - 190, 50.0F);
+            GlStateManager.scale(30, 30, 30);
+            mc.getTextureManager().bindTexture(mc.player.getLocationSkin());
+            GlStateManager.enableColorMaterial();
+            RenderHelper.enableStandardItemLighting();
+            biped.bipedHeadwear.rotateAngleX = biped.bipedHead.rotateAngleX = 0.7f;
+            biped.bipedHeadwear.rotateAngleY = biped.bipedHead.rotateAngleY = (float) (-Math.PI / 4);
+            biped.bipedHeadwear.rotateAngleZ = biped.bipedHead.rotateAngleZ = -0.5f;
+            biped.bipedHead.render(0.064f);
+            biped.bipedHeadwear.render(0.0625F);
+            GlStateManager.color(1, 1, 1, 1);
+            GlStateManager.popMatrix();
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+            GlStateManager.disableTexture2D();
+            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+
+            //Entity and info
             if (mc.player != null && mouseOver != null) {
+
+                GlStateManager.pushMatrix();
+                drawEntityOnScreen(mc.displayWidth / 2 - 110, mc.displayHeight / 2 - 80, 50, mouseOver);
+                GlStateManager.popMatrix();
+
                 mc.fontRenderer.drawStringWithShadow("Name: " + mouseOver.getName(), resolution.getScaledWidth() - 160, resolution.getScaledHeight() - 70, Color.GREEN.getRGB());
                 mc.fontRenderer.drawStringWithShadow("Distance: " + Math.round(mouseOver.getDistance(mc.player)) + " blocks", resolution.getScaledWidth() - 160, resolution.getScaledHeight() - 60, Color.GREEN.getRGB());
                 mc.fontRenderer.drawStringWithShadow("Age: " + mouseOver.ticksExisted + " ticks", resolution.getScaledWidth() - 160, resolution.getScaledHeight() - 50, Color.GREEN.getRGB());
@@ -31,11 +90,6 @@ public class OverlaySonicShades implements IOverlay {
             }
             GlStateManager.popMatrix();
         }
-    }
-
-    @Override
-    public void renderUpdate() {
-
     }
 
 
