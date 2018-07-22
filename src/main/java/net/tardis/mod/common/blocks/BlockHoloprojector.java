db6a923ffb021f669dce01309dc6890e004153cc
Spectre0987 <spencecd2@gmail.com>
2018-07-22 00:55:48 -0700

    Removed Block Rendering on Holoprojector

--- src/main/java/net/tardis/mod/common/blocks/BlockHoloprojector.java
+++ src/main/java/net/tardis/mod/common/blocks/BlockHoloprojector.java
@@ -3,6 +3,7 @@ package net.tardis.mod.common.blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumBlockRenderType;
 import net.tardis.mod.Tardis;
 import net.tardis.mod.common.tileentity.TileEntityHoloprojector;
 
@@ -23,6 +24,11 @@ public class BlockHoloprojector extends BlockTileBase {
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
+	}
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.INVISIBLE;
 	}
 
 }
