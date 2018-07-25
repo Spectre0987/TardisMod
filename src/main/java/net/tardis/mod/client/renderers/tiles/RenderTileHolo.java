b0c447181cddad053a8393b0d1d7203b9df76b25
Spectre0987 <spencecd2@gmail.com>
2018-07-24 22:05:56 -0700

    Added Temp Model for Holoprojector

--- src/main/java/net/tardis/mod/client/renderers/tiles/RenderTileHolo.java
+++ src/main/java/net/tardis/mod/client/renderers/tiles/RenderTileHolo.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.tardis.mod.client.worldshell.RenderWorldShell;
 import net.tardis.mod.common.tileentity.TileEntityHoloprojector;
+import net.tardis.mod.util.helpers.Helper;
 
 public class RenderTileHolo extends TileEntitySpecialRenderer<TileEntityHoloprojector> {
 
@@ -16,7 +17,7 @@ public class RenderTileHolo extends TileEntitySpecialRenderer<TileEntityHoloproj
 	public void render(TileEntityHoloprojector te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		Minecraft mc = Minecraft.getMinecraft();
 		GlStateManager.pushMatrix();
-		GlStateManager.translate(x + 0.4688, y + 1, z + 0.4688);
+		GlStateManager.translate(x + 0.4688, y + Helper.precentToPixels(6), z + 0.4688);
 		GlStateManager.scale(0.06,0.06,0.06);
 		try {
 			mc.entityRenderer.disableLightmap();
