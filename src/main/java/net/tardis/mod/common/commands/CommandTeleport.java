e27cde30352c36c63d37fb76d55adb935968ce9b
Spectre0987 <spencecd2@gmail.com>
2018-08-11 21:01:09 -0700

    Stoped de-sync and death when rifing an entity and using commands

--- src/main/java/net/tardis/mod/common/commands/CommandTeleport.java
+++ src/main/java/net/tardis/mod/common/commands/CommandTeleport.java
@@ -6,14 +6,12 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.server.permission.PermissionAPI;
 import net.tardis.mod.common.dimensions.TDimensions;
 import net.tardis.mod.common.strings.TStrings;
-import net.tardis.mod.util.TardisTeleporter;
-import net.tardis.mod.util.helpers.Helper;
+import net.tardis.mod.common.tileentity.TileEntityTardis;
 import net.tardis.mod.util.helpers.TardisHelper;
 
 public class CommandTeleport extends CommandBase {
@@ -33,8 +31,8 @@ public class CommandTeleport extends CommandBase {
     	EntityPlayerMP player = CommandBase.getCommandSenderAsPlayer(sender);
         if (TardisHelper.hasTardis(player.getUniqueID())){
             BlockPos pos = TardisHelper.getTardis(player.getUniqueID());
-            player.connection.setPlayerLocation(pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, Helper.get360FromFacing(EnumFacing.EAST), 0);
-            if(player.dimension != TDimensions.TARDIS_ID)server.getPlayerList().transferPlayerToDimension(player, TDimensions.TARDIS_ID,new TardisTeleporter());
+            player.dismountRidingEntity();
+            ((TileEntityTardis)server.getWorld(TDimensions.TARDIS_ID).getTileEntity(pos)).enterTARDIS(player);
         }
         else {
             sender.sendMessage(new TextComponentTranslation(TStrings.Commands.NO_TARDIS_OWNED));
