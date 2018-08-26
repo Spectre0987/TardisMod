a1a57a03a9aefc3bb5fae1ce28b24a0a5beff450
LotuxPunk <clement@vandendaelen.com>
2018-08-27 01:22:41 +0200

    fix render

--- src/main/java/net/tardis/mod/client/renderers/entities/RenderAdipose.java
+++ src/main/java/net/tardis/mod/client/renderers/entities/RenderAdipose.java
@@ -1,36 +1,40 @@
 package net.tardis.mod.client.renderers.entities;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
+import net.tardis.mod.Tardis;
 import net.tardis.mod.client.models.entity.ModelAdipose;
 import net.tardis.mod.common.entities.EntityAdipose;
+import net.tardis.mod.common.entities.EntityCybermanInvasion;
 
 import javax.annotation.Nullable;
 
 public class RenderAdipose extends RenderLiving<EntityAdipose> {
-    ModelAdipose adipose = new ModelAdipose();
+    Minecraft mc;
+    public ModelAdipose adipose = new ModelAdipose();
+    public static final ResourceLocation TEXTURE = new ResourceLocation(Tardis.MODID, "textures/entity/mob/adipose.png");
 
     public RenderAdipose(RenderManager rendermanagerIn) {
-        super(rendermanagerIn, new ModelAdipose(), 1.0F);
+        super(rendermanagerIn, new ModelAdipose(), 0.1F);
+        mc = Minecraft.getMinecraft();
     }
 
     @Override
-    protected void renderModel(EntityAdipose entitylivingbaseIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
+    public void doRender(EntityAdipose entity, double x, double y, double z, float entityYaw, float partialTicks) {
         GlStateManager.pushMatrix();
-        adipose.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        GlStateManager.translate(x, y + 1.5 , z);
+        mc.getTextureManager().bindTexture(TEXTURE);
+        GlStateManager.rotate(180,1,0,0);
+        adipose.render(entity, entity.limbSwing, entity.limbSwingAmount, entity.ticksExisted,entity.getRotationYawHead(), entity.rotationPitch, 0.0625F);
         GlStateManager.popMatrix();
     }
 
-    /**
-     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
-     *
-     * @param entity
-     */
     @Nullable
     @Override
     protected ResourceLocation getEntityTexture(EntityAdipose entity) {
-        return null;
+        return TEXTURE;
     }
 }
