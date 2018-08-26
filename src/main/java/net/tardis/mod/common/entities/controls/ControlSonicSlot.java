7264851ca2ef544ff6ad993754499253db487d98
Spectre0987 <spencecd2@gmail.com>
2018-08-26 11:55:28 -0700

    Added A sonic screwdriver port on the console

--- src/main/java/net/tardis/mod/common/entities/controls/ControlSonicSlot.java
+++ src/main/java/net/tardis/mod/common/entities/controls/ControlSonicSlot.java
@@ -2,15 +2,20 @@ package net.tardis.mod.common.entities.controls;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.tardis.mod.common.items.TItems;
+import net.tardis.mod.common.items.ItemSonic;
 import net.tardis.mod.common.tileentity.TileEntityTardis;
 import net.tardis.mod.util.helpers.Helper;
 
 public class ControlSonicSlot extends EntityControl {
 	
-	private ItemStack slot = new ItemStack(TItems.sonic_screwdriver);
+	public static final DataParameter<NBTTagCompound> ITEM = EntityDataManager.createKey(ControlSonicSlot.class, DataSerializers.COMPOUND_TAG);
 	
 	public ControlSonicSlot(TileEntityTardis tardis) {
 		super(tardis);
@@ -23,12 +28,32 @@ public class ControlSonicSlot extends EntityControl {
 	
 	@Override
 	public Vec3d getOffset(TileEntityTardis tardis) {
-		return Helper.convertToPixels(0, 0, 0);
+		return Helper.convertToPixels(0, 16, 0);
 	}
 	
 	@Override
 	public void preformAction(EntityPlayer player) {
-		
+		if(player.getHeldItemMainhand().getItem() instanceof ItemSonic && this.getItemStack().isEmpty()) {
+			this.setItemStack(player.getHeldItemMainhand());
+			player.setHeldItem(EnumHand.MAIN_HAND, ItemStack.EMPTY);
+		}
+		else if(player.getHeldItemMainhand().isEmpty() && !this.getItemStack().isEmpty()){
+			player.setHeldItem(EnumHand.OFF_HAND, this.getItemStack());
+		}
+	}
+
+	@Override
+	protected void entityInit() {
+		super.entityInit();
+		this.getDataManager().register(ITEM, ItemStack.EMPTY.writeToNBT(new NBTTagCompound()));
+	}
+	
+	public void setItemStack(ItemStack stack) {
+		this.getDataManager().set(ITEM, stack.writeToNBT(new NBTTagCompound()));
+	}
+	
+	public ItemStack getItemStack() {
+		return new ItemStack(this.getDataManager().get(ITEM));
 	}
 	
 }
