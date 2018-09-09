4133df02035b8c0bd7b10811d2c5cded65326b42
Spectre0987 <spencecd2@gmail.com>
2018-09-09 12:41:13 -0700

    Added Mushroom exterior

--- src/main/java/net/tardis/mod/client/renderers/exteriors/RenderTileDoorCC.java
+++ src/main/java/net/tardis/mod/client/renderers/exteriors/RenderTileDoorCC.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.Vec3d;
 import net.tardis.mod.Tardis;
 import net.tardis.mod.client.models.ModelBlocks;
 import net.tardis.mod.client.renderers.RenderHelper;
@@ -15,9 +16,10 @@ import net.tardis.mod.util.helpers.Helper;
 
 public class RenderTileDoorCC extends TileEntitySpecialRenderer<TileEntityDoor> {
 
-	public static ModelBlocks model = new ModelBlocks(new ResourceLocation(Tardis.MODID, "shells/tree.json"));
+	public static ModelBlocks model = new ModelBlocks(new ResourceLocation(Tardis.MODID, "shells/cactus.json"));
 	public static final ResourceLocation CACTUS = new ResourceLocation(Tardis.MODID, "shells/cactus.json");
 	public static final ResourceLocation TREE = new ResourceLocation(Tardis.MODID, "shells/tree.json");
+	public static final ResourceLocation MUSHROOM = new ResourceLocation(Tardis.MODID, "shells/mushroom.json");
 	
 	RenderWorldShell renderShell;
 	
@@ -29,13 +31,17 @@ public class RenderTileDoorCC extends TileEntitySpecialRenderer<TileEntityDoor> 
 	public void render(TileEntityDoor te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y - 1, z);
-		ResourceLocation rl = model.loc;
 		String name = te.getWorld().getBiome(te.getPos()).getBiomeName().toLowerCase();
-		if(name.contains("desert") && !model.loc.equals(CACTUS)) model = new ModelBlocks(CACTUS);
-		if(name.contains("taiga") && !model.loc.equals(TREE)) model = new ModelBlocks(TREE);
-		if(rl != null) {
+		ResourceLocation rl = null;
+		if(name.contains("forest") || name.contains("plains")) rl = TREE;
+		else if(name.contains("desert")) rl = CACTUS;
+		else if(name.contains("mushroom")) rl = MUSHROOM;
+		else rl = CACTUS;
+		if(!model.getKey().equals(rl) && rl != null) model = new ModelBlocks(rl);
+		if(model != null && model.getKey() != null) {
 			model.render();
 		}
+		else System.err.println("No Model!");
 		IBlockState state = te.getWorld().getBlockState(te.getPos());
 		if(state.getBlock() instanceof BlockTardisTop) {
 			EnumFacing facing = state.getValue(BlockTardisTop.FACING);
@@ -43,10 +49,11 @@ public class RenderTileDoorCC extends TileEntitySpecialRenderer<TileEntityDoor> 
 			if(facing == EnumFacing.EAST || facing == EnumFacing.WEST) {
 				GlStateManager.rotate(180, 0, 1, 0);
 			}
-			if(facing == EnumFacing.WEST) GlStateManager.translate(-1.01, 0, 0);
-			if(facing == EnumFacing.EAST) GlStateManager.translate(0, 0, -1.01);
-			if(facing == EnumFacing.SOUTH) GlStateManager.translate(-1.01, 0, -1.01);
-			if(!te.isLocked())RenderHelper.renderPortal(renderShell, te, partialTicks, 0);
+			if(facing == EnumFacing.WEST) GlStateManager.translate(-0.875, 0, -0.001);
+			if(facing == EnumFacing.EAST) GlStateManager.translate(0.125, 0, -1.001);
+			if(facing == EnumFacing.SOUTH) GlStateManager.translate(-0.875, 0, -1.01);
+			if(facing == EnumFacing.NORTH) GlStateManager.translate(0.125, 0, -0.001);
+			if(!te.isLocked())RenderHelper.renderPortal(renderShell, te, partialTicks, 0, null, new Vec3d(0.75, 1.8, 0));
 		}
 		GlStateManager.popMatrix();
 	}
