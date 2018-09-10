cbfdc9926774a32fc42d6b785913ad2fb176e130
Spectre0987 <spencecd2@gmail.com>
2018-09-10 07:16:26 -0700

    Made Active Camo damage the circuit more

--- src/main/java/net/tardis/mod/common/systems/SystemCCircuit.java
+++ src/main/java/net/tardis/mod/common/systems/SystemCCircuit.java
@@ -3,13 +3,26 @@ package net.tardis.mod.common.systems;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.tardis.mod.common.blocks.TBlocks;
 import net.tardis.mod.common.items.TItems;
 import net.tardis.mod.common.systems.TardisSystems.ISystem;
+import net.tardis.mod.common.tileentity.TileEntityTardis;
+import net.tardis.mod.util.helpers.Helper;
 
 public class SystemCCircuit extends ISystem{
 
+	boolean checkActive = false;
+	
 	@Override
-	public void onUpdate(World world, BlockPos consolePos) {}
+	public void onUpdate(World world, BlockPos consolePos) {
+		if(checkActive) {
+			TileEntityTardis tardis = Helper.getTardis(world.getTileEntity(consolePos));
+			if(tardis != null && tardis.getTopBlock().getBlock() == TBlocks.tardis_top_cc) {
+				this.setHealth(this.getHealth() - 0.01F);
+				checkActive = false;
+			}
+		}
+	}
 
 	@Override
 	public void damage() {}
