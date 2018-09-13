991e34ef1ddf9e4a1f6b6ea994a4a574d0db8ec6
Sub <SandedShoes@gmail.com>
2018-09-13 17:12:18 +0100

    Repairs to angels intergration

--- src/main/java/net/tardis/mod/integrations/WeepingAngel.java
+++ src/main/java/net/tardis/mod/integrations/WeepingAngel.java
@@ -1,6 +1,20 @@
 package net.tardis.mod.integrations;
 
+import me.sub.angels.common.entities.EntityWeepingAngel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.tardis.mod.common.blocks.BlockConsole;
+import net.tardis.mod.common.dimensions.TDimensions;
+import net.tardis.mod.common.items.ItemKey;
+import net.tardis.mod.common.tileentity.TileEntityTardis;
 
 public class WeepingAngel {
 
@@ -8,12 +22,12 @@ public class WeepingAngel {
 		MinecraftForge.EVENT_BUS.register(new WeepingAngel());
 	}
 
-    // TODO - Re-write to work with new angel code, shouldn't take long
-    /*@SubscribeEvent
+
+	@SubscribeEvent
 	public void hijackAngel(LivingEvent.LivingUpdateEvent event) {
-		if(event.getEntityLiving() instanceof EntityAngel) {
+		if (event.getEntityLiving() instanceof EntityWeepingAngel) {
 			if(!event.getEntityLiving().world.isRemote) {
-				EntityAngel angel = (EntityAngel)event.getEntityLiving();
+				EntityWeepingAngel angel = (EntityWeepingAngel) event.getEntityLiving();
     if(event.getEntityLiving().dimension != TDimensions.TARDIS_ID) {
 					ItemStack stack = angel.getHeldItemMainhand();
 					if(stack.getItem() instanceof ItemKey) {
@@ -29,19 +43,22 @@ public class WeepingAngel {
 							}
 						}
 					}
-				}
-				else {
-					AxisAlignedBB bb = angel.getEntityBoundingBox().grow(16D);
+	} else {
+		AxisAlignedBB bb = angel.getEntityBoundingBox().grow(30D);
 					World world = angel.world;
 					for(int x = (int)bb.minX; x < bb.maxX; ++x) {
 						for(int y = (int)bb.minY; y < bb.maxY; ++y) {
 							for(int z = (int)bb.minZ; z < bb.maxZ; ++z) {
 								BlockPos newPos = new BlockPos(x,y,z);
 								if(world.getBlockState(newPos).getBlock() instanceof BlockConsole) {
+									angel.setPose("POINT");
 									TileEntityTardis tardis = ((TileEntityTardis)world.getTileEntity(newPos));
 									if(tardis.fuel > 0.001) {
+										angel.setHungerLevel(angel.getHungerLevel() + 1);
 										tardis.fuel -= 0.001;
 										tardis.markDirty();
+									} else {
+
 									}
 								}
 							}
@@ -51,5 +68,5 @@ public class WeepingAngel {
 			}
 		}
 	}
-     **/
+
 }
