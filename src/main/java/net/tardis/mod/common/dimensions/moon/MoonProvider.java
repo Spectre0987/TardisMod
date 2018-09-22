8d643c104386ab0e95b378b44f724d303cbc05aa
Spectre0987 <spencecd2@gmail.com>
2018-09-22 08:24:14 -0700

    Added Moon gravity

--- src/main/java/net/tardis/mod/common/dimensions/moon/MoonProvider.java
+++ src/main/java/net/tardis/mod/common/dimensions/moon/MoonProvider.java
@@ -1,6 +1,7 @@
 package net.tardis.mod.common.dimensions.moon;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
@@ -8,6 +9,9 @@ import net.minecraft.world.biome.BiomeProvider;
 import net.minecraft.world.biome.BiomeProviderSingle;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.client.IRenderHandler;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.tardis.mod.client.renderers.sky.SkyRendererMoon;
@@ -64,6 +68,33 @@ public class MoonProvider extends WorldProvider{
 	@Override
 	public float[] calcSunriseSunsetColors(float celestialAngle, float partialTicks) {
 		return new float[] {0, 0, 0, 0};
+	}
+
+	@Override
+	public void onPlayerAdded(EntityPlayerMP player) {
+		super.onPlayerAdded(player);
+		player.setNoGravity(true);
+	}
+
+	@Override
+	public void onPlayerRemoved(EntityPlayerMP player) {
+		super.onPlayerRemoved(player);
+		player.setNoGravity(false);
+	}
+	
+	@EventBusSubscriber
+	public static class Events{
+		
+		@SubscribeEvent
+		public static void grav(LivingUpdateEvent event) {
+			if(event.getEntityLiving().dimension == TDimensions.MOON_ID) {
+				if(!event.getEntityLiving().onGround) {
+					event.getEntityLiving().motionY -= 0.01;
+					event.getEntityLiving().fallDistance *= 0.01D;
+				}
+			}
+		}
+		
 	}
 
 }
