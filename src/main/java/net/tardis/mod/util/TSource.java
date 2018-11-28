c86d0b9c4f8a0395ede4eb45c3fbf668d26ab55c
LotuxPunk <clement@vandendaelen.com>
2018-11-28 09:44:06 +0100

    Use constructors properly ! :D

--- src/main/java/net/tardis/mod/util/TSource.java
+++ src/main/java/net/tardis/mod/util/TSource.java
@@ -10,15 +10,14 @@ public class TSource extends DamageSource {
     private String message;
     private boolean blockable;
 
-    public TSource(String name) {
-        this(name, false);
-        this.message = "damagesrc.tardis." + name;
-    }
-
     public TSource(String name, boolean blockable) {
         super(name);
         this.message = "damagesrc.tardis." + name;
         this.blockable = blockable;
+    }
+    
+    public TSource(String name) {
+        this(name, false);
     }
 
     @Override
