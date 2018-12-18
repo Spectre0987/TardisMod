d5aeeb3086207501ee8bfdcc6362cf3b8b785e9a
Spectre0987 <spencecd2@gmail.com>
2018-12-18 00:09:00 -0600

    Added hardness to monitor and made it drop itself

--- src/main/java/net/tardis/mod/common/blocks/BlockMonitor.java
+++ src/main/java/net/tardis/mod/common/blocks/BlockMonitor.java
@@ -1,5 +1,7 @@
 package net.tardis.mod.common.blocks;
 
+import java.util.function.Supplier;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,7 +10,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.storage.MapData;
@@ -17,8 +21,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.tardis.mod.client.guis.GuiProtocol;
 import net.tardis.mod.common.tileentity.TileEntityTardis;
 import net.tardis.mod.util.common.helpers.TardisHelper;
-
-import java.util.function.Supplier;
 
 public class BlockMonitor extends BlockFacingDecoration {
 
@@ -49,6 +51,12 @@ public class BlockMonitor extends BlockFacingDecoration {
 	@SideOnly(Side.CLIENT)
 	public void openGui(TileEntityTardis tardis) {
 		Minecraft.getMinecraft().displayGuiScreen(new GuiProtocol(tardis.getPos()));
+	}
+
+	@Override
+	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+		drops.add(new ItemStack(this));
+		super.getDrops(drops, world, pos, state, fortune);
 	}
 
 }
