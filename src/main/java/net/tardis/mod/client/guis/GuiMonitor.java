fa801b9e6a998cf030a84698dc586b3a2ec56501
honeyluck <jakeross080@gmail.com>
2019-01-13 16:30:21 +0000

    "Fixed" Text size

--- src/main/java/net/tardis/mod/client/guis/GuiMonitor.java
+++ src/main/java/net/tardis/mod/client/guis/GuiMonitor.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNo;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -95,6 +96,8 @@ public class GuiMonitor extends GuiScreen {
 		ResourceLocation TEXTURE = new ResourceLocation(Tardis.MODID, "textures/gui/monitor_ui.png");
 		int BUTTON_WIDTH = 75;
 		int BUTTON_HEIGHT = 32;
+		int colour = 0x00938F;
+		float scale;
 		
 		public MonitorButton(int buttonId, int x, int y, String buttonText) {
 			super(buttonId, x, y, x, 32, buttonText);
@@ -104,11 +107,18 @@ public class GuiMonitor extends GuiScreen {
 		public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
 			if(visible) {
 				FontRenderer fontRenderer = mc.fontRenderer;
+				if(fontRenderer.getStringWidth(this.displayString) > 70) {
+					scale = (float) 70 / fontRenderer.getStringWidth(this.displayString);
+				} else {
+					scale = 1;
+				}
 				mc.getTextureManager().bindTexture(TEXTURE);
 				if(mouseX >= this.x && mouseX <= this.x + BUTTON_WIDTH && mouseY >= this.y && mouseY <= this.y + BUTTON_HEIGHT) {
 					hovered = true;
+					colour = 0xCE8F23;
 				} else {
 					hovered = false;
+					colour = 0x00938F;
 				}
 				
 				if(hovered){
@@ -116,7 +126,12 @@ public class GuiMonitor extends GuiScreen {
 				} else {
 					this.drawTexturedModalRect(this.x, this.y, 7, 209, BUTTON_WIDTH, BUTTON_HEIGHT);
 				}
-				this.drawString(fontRenderer, this.displayString, this.x, this.y + 10, 0xFFFFFF);
+				GlStateManager.pushMatrix();
+				GlStateManager.scale(scale, scale, 1);
+				this.drawCenteredString(fontRenderer, this.displayString, (int) ((1 / scale) * (this.x + 37)), (int) ((1 / scale) * (this.y + 16)), colour);
+				GlStateManager.color(1, 1, 1);
+				GlStateManager.scale(1, 1, 1);
+				GlStateManager.popMatrix();
 			}
 		}
 		
