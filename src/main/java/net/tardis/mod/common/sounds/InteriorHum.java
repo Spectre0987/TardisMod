f0076a3099c505e4a9669cb9d8374535be8db59a
Suffril <sandedshoes@gmail.com>
2019-01-22 11:46:14 +0000

    A start on sound stuff

--- src/main/java/net/tardis/mod/common/sounds/InteriorHum.java
+++ src/main/java/net/tardis/mod/common/sounds/InteriorHum.java
@@ -7,24 +7,18 @@ import java.util.List;
 
 public class InteriorHum {
 
-	public static InteriorHum DEFAULT = new InteriorHum(TSounds.interior_hum_80, 20);
-	public static InteriorHum INTERIOR_HUM_1963 = new InteriorHum(TSounds.INTERIOR_HUM_1963, 50);
+	public static InteriorHum DEFAULT = new InteriorHum(TSounds.interior_hum_80);
+	public static InteriorHum INTERIOR_HUM_1963 = new InteriorHum(TSounds.INTERIOR_HUM_1963);
 
 	public static List<InteriorHum> hums = Arrays.asList(DEFAULT, INTERIOR_HUM_1963);
 
-	private SoundEvent event = null;
-	private int timeToPlay = 0;
+	private SoundEvent event;
 
-	public InteriorHum(SoundEvent event, int time) {
+	public InteriorHum(SoundEvent event) {
 		this.event = event;
-		this.timeToPlay = time;
 	}
 
 	public SoundEvent getSound() {
 		return this.event;
-	}
-
-	public int getTicks() {
-		return this.timeToPlay;
 	}
 }
