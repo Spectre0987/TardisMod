c1de7ea6b890b5d63d5fcc82c3b846b1b5653b4b
Suffril <sandedshoes@gmail.com>
2019-02-06 19:57:49 +0000

    Fixed dumb teleporter

--- src/main/java/net/tardis/mod/util/common/helpers/Helper.java
+++ src/main/java/net/tardis/mod/util/common/helpers/Helper.java
@@ -48,8 +48,7 @@ public class Helper {
 	}
 
 	public static void transferToWorld(EntityPlayerMP player, WorldServer world, BlockPos pos, int dim) {
-		world.getMinecraftServer().getPlayerList().transferPlayerToDimension(player, dim, new TardisTeleporter(world));
-		player.setPositionAndUpdate(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+		world.getMinecraftServer().getPlayerList().transferPlayerToDimension(player, dim, new TardisTeleporter(new BlockPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5)));
 	}
 
 	public static Vec3d convertToPixels(Vec3d vec) {
