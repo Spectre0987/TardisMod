afe367a4da99a162c46bb209ff51f4a28f8a7150
Spectre0987 <spencecd2@gmail.com>
2019-02-07 18:26:21 -0600

    Console Room now generates. (Might be important)

--- src/main/java/net/tardis/mod/common/tileentity/TileEntityTardisCoral.java
+++ src/main/java/net/tardis/mod/common/tileentity/TileEntityTardisCoral.java
@@ -81,7 +81,7 @@ public class TileEntityTardisCoral extends TileEntity implements ITickable {
 			TileEntity te = tardisWorld.getTileEntity(pos);
 			if (te == null || !(te instanceof TileEntityTardis)) {
 				tardisWorld.setBlockState(pos, TBlocks.console_02.getDefaultState());
-				ConsoleRoom.CONSOLE_ROOMS.get(1).generate((WorldServer)world, pos);
+				ConsoleRoom.CONSOLE_ROOMS.get(1).generate(((WorldServer)world).getMinecraftServer().getWorld(TDimensions.TARDIS_ID), pos);
 				TileEntityTardis tardis = (TileEntityTardis) tardisWorld.getTileEntity(pos);
 				this.getWorld().setBlockState(this.getPos(), Blocks.AIR.getDefaultState());
 				tardis.setAbsoluteDesination(getPos(), this.getWorld().provider.getDimension());
