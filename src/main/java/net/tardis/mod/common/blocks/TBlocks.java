5ea9d51e847cbf1a248fffd0547c5d4276a6427a
Suffril <sandedshoes@gmail.com>
2019-02-09 22:09:09 +0000

    Sorted out grass

--- src/main/java/net/tardis/mod/common/blocks/TBlocks.java
+++ src/main/java/net/tardis/mod/common/blocks/TBlocks.java
@@ -130,10 +130,10 @@ public class TBlocks {
 	public static Block moon_dirt = register(new BlockBase(), "moon_dirt");
 
 	// Gallifrey
-	public static Block gallifreyan_grass = register(new BlockGallifreyDirt(true), "gallifreyan_grass", false);
-	public static Block gallifreyan_dirt = register(new BlockGallifreyDirt(false), "gallifreyan_dirt", false);
+	public static Block gallifreyan_grass = register(new BlockGrassBase(), "gallifreyan_grass", false).setCreativeTab(TardisTabs.GALLIFREY_BLOCKS);
+	public static Block gallifreyan_dirt = register(new BlockGallifreyDirt(false), "gallifreyan_dirt", false).setCreativeTab(TardisTabs.GALLIFREY_BLOCKS);
 
-	public static Block gallifreyan_stone = register(new BlockGallifreyStone(1.5F, 30F), "gallifreyan_stone", false);
+	public static Block gallifreyan_stone = register(new BlockGallifreyStone(1.5F, 30F), "gallifreyan_stone", false).setCreativeTab(TardisTabs.GALLIFREY_BLOCKS);
 
 
 	public static Block suitcase = register(new BlockSuitcase(), "suitcase");
