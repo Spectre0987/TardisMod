0a911d6ff7ecd30dbcee9f90e9ab92b96cdbcead
Josia <38124619+Josia50@users.noreply.github.com>
2019-02-11 07:58:03 +0100

    ConsoleRoom Fix

--- src/main/java/net/tardis/mod/common/ars/ConsoleRoom.java
+++ src/main/java/net/tardis/mod/common/ars/ConsoleRoom.java
@@ -18,45 +18,50 @@ import net.tardis.mod.common.tileentity.TileEntityTardis;
 
 public class ConsoleRoom {
 
-	public static List<ConsoleRoom> CONSOLE_ROOMS = new ArrayList<>();
-	
-	private ResourceLocation preview;
-	private ResourceLocation filePath;
-	private BlockPos consolePos = BlockPos.ORIGIN;
-	
-	public ConsoleRoom(ResourceLocation preview, ResourceLocation filePath, BlockPos cp) {
-		this.preview = preview;
-		this.filePath = filePath;
-		this.consolePos = cp;
-	}
-	
-	public ResourceLocation getPreview() {
-		return this.preview;
-	}
-	
-	public ResourceLocation getFilePath() {
-		return this.filePath;
-	}
-	
-	public void generate(WorldServer world, BlockPos pos) {
-		for(Entity entity : world.getEntitiesWithinAABB(Entity.class, Block.FULL_BLOCK_AABB.offset(pos).grow(20))) {
-			if(!(entity instanceof EntityControl) || !(entity instanceof EntityLivingBase)) {
-				entity.setDead();
-				world.updateEntities(); 
-				world.updateEntityWithOptionalForce(entity, true);
-			}
-		}
-		Template temp = world.getStructureTemplateManager().get(world.getMinecraftServer(), filePath);
-		PlacementSettings ps = new PlacementSettings();
-		IBlockState consoleState = world.getBlockState(pos);
-		TileEntityTardis tardis = (TileEntityTardis)world.getTileEntity(pos);
-		temp.addBlocksToWorld(world, pos.subtract(consolePos), ps);
-		world.setBlockState(pos, consoleState);
-		if(tardis != null)
-			((TileEntityTardis)world.getTileEntity(pos)).readFromNBT(tardis.writeToNBT(new NBTTagCompound()));
-	}
-	
-	public BlockPos getConsolePos(){
-		return this.consolePos;
-	}
+    public static List<ConsoleRoom> CONSOLE_ROOMS = new ArrayList<>();
+
+    private ResourceLocation preview;
+    private ResourceLocation filePath;
+    private BlockPos consolePos = BlockPos.ORIGIN;
+
+    public ConsoleRoom(ResourceLocation preview, ResourceLocation filePath, BlockPos cp) {
+        this.preview = preview;
+        this.filePath = filePath;
+        this.consolePos = cp;
+    }
+
+    public ResourceLocation getPreview() {
+        return this.preview;
+    }
+
+    public ResourceLocation getFilePath() {
+        return this.filePath;
+    }
+
+    public void generate(WorldServer world, BlockPos pos) {
+        IBlockState consoleState = world.getBlockState(pos);
+        TileEntityTardis tardis = (TileEntityTardis) world.getTileEntity(pos);
+        if (!tardis.isInFlight()) {
+            for (Entity entity : world.getEntitiesWithinAABB(Entity.class, Block.FULL_BLOCK_AABB.offset(pos).grow(20))) {
+                if (!(entity instanceof EntityControl) || !(entity instanceof EntityLivingBase)) {
+                    entity.setDead();
+                    world.updateEntities();
+                    world.updateEntityWithOptionalForce(entity, true);
+                }
+                if (entity instanceof EntityLivingBase)
+                    tardis.transferPlayer(entity, false);
+            }
+
+            Template temp = world.getStructureTemplateManager().get(world.getMinecraftServer(), filePath);
+            PlacementSettings ps = new PlacementSettings();
+            temp.addBlocksToWorld(world, pos.subtract(consolePos), ps);
+            world.setBlockState(pos, consoleState);
+            if (tardis != null)
+                ((TileEntityTardis) world.getTileEntity(pos)).readFromNBT(tardis.writeToNBT(new NBTTagCompound()));
+        }
+    }
+
+    public BlockPos getConsolePos() {
+        return this.consolePos;
+    }
 }
