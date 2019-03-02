d479c3b2b2a7a98238eff1d09d5d4be32d3f262a
Suffril <sandedshoes@gmail.com>
2019-03-02 21:52:04 +0000

    ewwwwwwwww

--- src/main/java/net/tardis/mod/common/tileentity/decoration/TileEntityHellbentMonitor.java
+++ src/main/java/net/tardis/mod/common/tileentity/decoration/TileEntityHellbentMonitor.java
@@ -3,9 +3,7 @@ package net.tardis.mod.common.tileentity.decoration;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityHellbentMonitor extends TileEntity {
-
 	public TileEntityHellbentMonitor() {
 	}
-
-
+	
 }
