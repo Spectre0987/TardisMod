3411941b1d2e7f8faa16197891b09190ae453d90
Spectre0987 <spencecd2@gmail.com>
2019-03-21 21:50:09 -0500

    Added Monitor shit and stoped the bug when you have doDaylightCycle set to false

--- src/main/java/net/tardis/mod/client/guis/GuiToggleHum.java
+++ src/main/java/net/tardis/mod/client/guis/GuiToggleHum.java
@@ -3,12 +3,13 @@ package net.tardis.mod.client.guis;
 import java.io.IOException;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.tardis.mod.Tardis;
-import net.tardis.mod.client.guis.elements.ButtonText;
+import net.tardis.mod.client.guis.elements.ButtonMonitor;
 import net.tardis.mod.common.sounds.InteriorHum;
 
 public class GuiToggleHum extends GuiScreen {
@@ -38,6 +39,9 @@ public class GuiToggleHum extends GuiScreen {
 		super.drawScreen(mouseX, mouseY, partialTicks);
 		Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURE);
 		this.drawTexturedModalRect(width / 2 - 242 / 2, height / 2 - 132 / 2, 0, 0, 242, 132);
+		FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
+		int titleWidth = fr.getStringWidth("Hum Selection");
+		fr.drawString("Hum Selection", width / 2 - titleWidth / 2, height / 2 - 50, 0xFFFFFF);
 		for(GuiButton button : this.buttonList) {
 			button.drawButton(Minecraft.getMinecraft(), mouseX, mouseY, partialTicks);
 		}
@@ -48,7 +52,7 @@ public class GuiToggleHum extends GuiScreen {
 		this.buttonList.clear();
 		int index = 1;
 		for(InteriorHum hum : InteriorHum.hums) {
-			this.addButton(new ButtonText(index, width / 2 - 60, (height / 2 + 40) - Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT * index, Minecraft.getMinecraft().fontRenderer, "> " + hum.getTranslatedName()));
+			this.addButton(new ButtonMonitor(index, width / 2 - 110, (height / 2 + 50) - Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT * index, "> " + hum.getTranslatedName()));
 			++index;
 		}
 	}
