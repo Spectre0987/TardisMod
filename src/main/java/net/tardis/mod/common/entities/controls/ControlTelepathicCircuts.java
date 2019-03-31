c9d56a40fea617be6cd035f48e271a777aba5d58
Suffril <sandedshoes@gmail.com>
2019-03-31 11:38:09 +0100

    Stopped the ability to use telepahic circuits when there is a pilot

--- src/main/java/net/tardis/mod/common/entities/controls/ControlTelepathicCircuts.java
+++ src/main/java/net/tardis/mod/common/entities/controls/ControlTelepathicCircuts.java
@@ -2,8 +2,10 @@ package net.tardis.mod.common.entities.controls;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -11,21 +13,26 @@ import net.tardis.mod.capability.CapabilityTardis;
 import net.tardis.mod.client.guis.GuiTelepathicCircuts;
 import net.tardis.mod.common.sounds.TSounds;
 import net.tardis.mod.common.tileentity.TileEntityTardis;
-import net.tardis.mod.common.tileentity.consoles.*;
+import net.tardis.mod.common.tileentity.consoles.TileEntityTardis01;
+import net.tardis.mod.common.tileentity.consoles.TileEntityTardis02;
+import net.tardis.mod.common.tileentity.consoles.TileEntityTardis03;
+import net.tardis.mod.common.tileentity.consoles.TileEntityTardis04;
+import net.tardis.mod.common.tileentity.consoles.TileEntityTardis05;
 import net.tardis.mod.util.common.helpers.Helper;
+import net.tardis.mod.util.common.helpers.PlayerHelper;
 
 public class ControlTelepathicCircuts extends EntityControl {
-
+	
 	public ControlTelepathicCircuts(TileEntityTardis tardis) {
 		super(tardis);
 		this.setSize(0.125F, 0.125F);
 	}
-
+	
 	public ControlTelepathicCircuts(World world) {
 		super(world);
 		this.setSize(0.125F, 0.125F);
 	}
-
+	
 	@Override
 	public Vec3d getOffset(TileEntityTardis tardis) {
 		if (tardis.getClass() == TileEntityTardis01.class || tardis.getClass() == TileEntityTardis02.class) {
@@ -33,29 +40,39 @@ public class ControlTelepathicCircuts extends EntityControl {
 		}
 		if (tardis instanceof TileEntityTardis03)
 			return Helper.convertToPixels(8.5, 2, 5.5);
-		if(tardis instanceof TileEntityTardis04)
+		if (tardis instanceof TileEntityTardis04)
 			return Helper.convertToPixels(0, -1, -8);
-		if(tardis instanceof TileEntityTardis05)
+		if (tardis instanceof TileEntityTardis05)
 			return Helper.convertToPixels(0, -2, 10.5);
 		return Helper.convertToPixels(10, -2, -6);
 	}
-
+	
 	@Override
 	public void preformAction(EntityPlayer player) {
-		if(player.isSneaking()){
-			CapabilityTardis.setupFlight(player);
-		} else {
-			if (world.isRemote) {
-				openGui();
+		if (!world.isRemote) {
+			TileEntity te = world.getTileEntity(this.getConsolePos());
+			if (te != null) {
+				TileEntityTardis tardis = (TileEntityTardis) te;
+				if (!tardis.hasPilot()) {
+					if (player.isSneaking()) {
+						CapabilityTardis.setupFlight(player);
+					} else {
+						if (world.isRemote) {
+							openGui();
+						}
+					}
+				} else {
+					PlayerHelper.sendMessage(player, new TextComponentTranslation("tardis.message.has_pilot"), true);
+				}
 			}
 		}
 	}
-
+	
 	@SideOnly(Side.CLIENT)
 	public void openGui() {
 		Minecraft.getMinecraft().displayGuiScreen(new GuiTelepathicCircuts(this.getConsolePos()));
 	}
-
+	
 	@Override
 	public void init(TileEntityTardis tardis) {
 		if (tardis != null) {
@@ -63,16 +80,16 @@ public class ControlTelepathicCircuts extends EntityControl {
 				setSize(Helper.precentToPixels(8F), Helper.precentToPixels(4F));
 			if (tardis.getClass() == TileEntityTardis.class)
 				this.setSize(Helper.precentToPixels(4F), Helper.precentToPixels(2F));
-			if(tardis instanceof TileEntityTardis04)
+			if (tardis instanceof TileEntityTardis04)
 				this.setSize(Helper.precentToPixels(6F), Helper.precentToPixels(2F));
-			if(tardis instanceof TileEntityTardis05)
+			if (tardis instanceof TileEntityTardis05)
 				this.setSize(Helper.precentToPixels(3F), Helper.precentToPixels(3F));
 		}
 	}
-
+	
 	@Override
 	public SoundEvent getUseSound() {
 		return TSounds.control_telepathic_circuit;
 	}
-
+	
 }
